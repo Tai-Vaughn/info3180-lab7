58bbaff061bb3f444385a96feec1b59aa6fa037a
Tai-Vaughn White <Isamutai200@gmail.com>
2020-04-30 12:29:27 -0500

    changes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -47,9 +47,12 @@ Vue.component('app-header', {
     
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
-          </li>
+            <li class="nav-item active">
+                <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+            </li>
+            <li class="nav-item active">
+                <router-link class="nav-link" to="/form">Form <span class="sr-only">(current)</span></router-link>
+            </li>
         </ul>
       </div>
     </nav>
